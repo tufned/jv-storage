988a03ec7332a7a2169287ea3b409e2a9e8a4dfd
Denys Funderat <funderatdenis@gmail.com>
2025-10-02 23:31:54 +0300

    StorageImpl fixed and improved

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,51 +1,59 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int SIZE = 10;
-    private static final int PAIR_SIZE = 2;
-    private final Object[][] storage;
+    private final K[] keys;
+    private final V[] values;
 
     public StorageImpl() {
-        this.storage = new Object[SIZE][PAIR_SIZE];
+        this.keys = (K[]) new Object[SIZE];
+        this.values = (V[]) new Object[SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        for (Object[] item : this.storage) {
-            if (Objects.equals(item[0], key)) {
-                item[1] = value;
-                return;
-            }
-        }
-        int size = size();
-        if (size == SIZE) {
+        int index = findKeyIndex(key);
+        if (index != -1) {
+            values[index] = value;
             return;
         }
-        this.storage[size() + 1] = new Object[]{ key, value };
+
+        int currentSize = size();
+        if (currentSize < SIZE) {
+            keys[currentSize] = key;
+            values[currentSize] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        for (Object[] item : this.storage) {
-            if (Objects.equals(item[0], key)) {
-                return (V) item[1];
-            }
+        int index = findKeyIndex(key);
+        if (index == -1) {
+            return null;
         }
-        return null;
+        return values[index];
     }
 
     @Override
     public int size() {
         int length = 0;
-        for (Object[] item : this.storage) {
-            if (item[0] == null && item[1] == null) {
-                continue;
+        for (int i = 0; i < SIZE; i++) {
+            if (keys[i] != null || values[i] != null) {
+                length++;
             }
-            length++;
         }
         return length;
+    }
+
+    private int findKeyIndex(K key) {
+        for (int i = 0; i < SIZE; i++) {
+            K itemKey = keys[i];
+            if (itemKey == key || (itemKey != null && itemKey.equals(key))) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
